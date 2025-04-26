b5f46ed0babdeee4ec7be01dd324e7362ce786d3
Shresth Srivastava <49335563+shresth2909@users.noreply.github.com>
2025-04-26 12:36:24 +0530

    Create data.py
    created and store content of quiz

---
+++ data.py
@@ -0,0 +1,48 @@
+question_data = [
+    {
+        "question":"Shaquille O&#039;Neal appeared in the 1997 film &quot;Space Jam&quot;.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "question":"In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "question":"The 2010 film &quot;The Social Network&quot; is a biographical drama film about MySpace founder Tom Anderson.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "question":"George Lucas directed the entire original Star Wars trilogy.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "question":"In the original script of &quot;The Matrix&quot;, the machines used humans as additional computing power instead of batteries.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "question":"In Alfred Hitchcock&#039;s film &#039;Psycho&#039; it is said he used chocolate syrup to simulate the blood in the famous shower scene from ",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]},
+    {
+        "question":"Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the movies Interstellar and The Martian.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]},
+     {
+        "question":"&quot;Foodfight!&quot; earned less than $80,000 at box office.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]},
+     {
+        "question":"Actor Tommy Chong served prison time.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]},
+     {
+        "question":"In the movie The Revenant, DiCaprio&#039;s character hunts down the killer of his son.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]}
+]
+
