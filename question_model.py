02532ad4131b2bcc1b07c928285605299ca0e96f
Shresth Srivastava <49335563+shresth2909@users.noreply.github.com>
2025-04-26 12:35:44 +0530

    Create question_model.py
    created Question class

---
+++ question_model.py
@@ -0,0 +1,10 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+'''
+text = Question(text='What\'sy your name?', answer='my name is shresth')
+print(text.text)
+print(text.answer)
+'''
